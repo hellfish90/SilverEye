d3711373bba2d8f5b25fc70af7ddd88821ad1490
Marc Solé Farré <marcsolefarre90@gmail.com>
2016-01-15 00:03:51 +0100

    add opener calls

--- silverEyeSentiment/opener.py
+++ silverEyeSentiment/opener.py
@@ -1,38 +1,133 @@
 
 import requests
-import uncurl
+import sys
+
+#URL_LANGUAGE_IDENTIFIER = "http://opener.olery.com/language-identifier"
+URL_LANGUAGE_IDENTIFIER = "http://192.168.101.127:1111"
+
+
+#URL_TOKENIZER = "http://opener.olery.com/tokenizer"
+URL_TOKENIZER = "http://192.168.101.127:2222"
+
+URL_POSTAGGER = "http://opener.olery.com/pos-tagger"
+#URL_POSTAGGER = "192.168.101.127:3333"
+
+URL_CONSTITUENCY_PARSE = "http://opener.olery.com/constituent-parser"
+#URL_CONSTITUENCY_PARSE = "192.168.101.127:4444"
+
+URL_POLARITY_TAGGER = "http://opener.olery.com/polarity-tagger"
+#URL_POLARITY_TAGGER = "192.168.101.127:5555"
+
+URL_PROPERTY_TAGGER = "http://opener.olery.com/property-tagger"
+#URL_POLARITY_TAGGER = "192.168.101.127:6666"
+
+URL_OPINION_DETECTOR = "http://opener.olery.com/opinion-detector"
+#URL_OPINION_DETECTOR = "192.168.101.127:7777"
+
+text = "el mundo se va a la mierda i aqui nadie hace nada maldito asco"
 
 
 
-URL_LANGUAGE_IDENTIFIER = "http://opener.olery.com/language-identifier"
-
-text = "hola hem dic marc i parlo catala"
-
-text_complet = "input=" + text + "&kaf=true"
-
-payload = {
-
-}
-
-headers = {'input': text_complet}
-
-def call_language_identifier():
-
-    r = requests.get(URL_LANGUAGE_IDENTIFIER, input=text_complet)
-    print r.status_code
-    print r.headers['content-type']
-    print r.encoding
-    print r.text
-    print r.json()
-
-def call_language_identifier2():
+def call_language_identifier(URL, data):
     session = requests.session()
-    r = requests.post(URL_LANGUAGE_IDENTIFIER, headers={}, data=text_complet, cookies={})
-    print r._content
+    text= complementByInputKAFTrue(data)
+
+    r = requests.post(URL, headers={}, data=text, cookies={})
+    return r._content
+
+def call_tokenizer(URL, data):
+    session = requests.session()
+    text= complementByInputKAFTrue(data).replace('"ca"','"es"')
+
+    r = requests.post(URL, headers={}, data=text, cookies={})
+    return r._content
+
+
+def call_pos_tagger(URL,data):
+    session = requests.session()
+    text= complementByInputKAFTrue(data).replace('"ca"','"es"')
+
+    r = requests.post(URL, headers={}, data=text, cookies={})
+    return r._content
+
+def call_constituency_parse(URL,data):
+    session = requests.session()
+    text= complementByInputKAFTrue(data).replace('"es"','"ca"')
+
+    r = requests.post(URL, headers={}, data=text, cookies={})
+    return r._content
+
+def call_polarity_tagger(URL,data):
+    session = requests.session()
+    text= complementByInputKAFTrue(data).replace('"ca"','"es"')
+
+    r = requests.post(URL, headers={}, data=text, cookies={})
+    return r._content
+
+def call_property_tagger(URL,data):
+    session = requests.session()
+    text= complementByInputKAFTrue(data).replace('"ca"','"es"')
+
+    r = requests.post(URL, headers={}, data=text, cookies={})
+    return r._content
+
+
+def call_opinion_detector(URL,data):
+    session = requests.session()
+    text= complementByInputKAFTrue(data).replace('"ca"','"es"')
+
+    r = requests.post(URL, headers={}, data=text, cookies={})
+    return r._content
+
+def complementByInputKAFTrue(text):
+    return "input=" + text + "&kaf=true"
+
+
+
+def get_polarity(data):
+    print "Positive:" + str(data.count('positive'))
+    print "Negative:" + str(data.count('negative'))
+    print "Neutral:" + str(data.count('neutral'))
+    print "--------------------------------------"
 
 if __name__ == '__main__':
 
+    text = raw_input("Introdueix el text a analitzar\n")
+
+    print "--------------------------------------"
+
     #print uncurl.parse("curl -d \"input=this is an english text&kaf=true\" http://localhost:9393 ")
 
-    call_language_identifier2()
+    output_language_identifier = call_language_identifier(URL_LANGUAGE_IDENTIFIER, complementByInputKAFTrue(text))
+    #print output_language_identifier
 
+    output_tokenizer = call_tokenizer(URL_TOKENIZER, output_language_identifier)
+    #print output_tokenizer
+
+    output_pos_tagger = call_pos_tagger(URL_POSTAGGER,output_tokenizer)
+    #print output_pos_tagger
+
+    output_constituency_parse = call_constituency_parse(URL_CONSTITUENCY_PARSE,output_pos_tagger)
+    #print output_constituency_parse
+
+
+    ''' Sentiment Analysis '''
+
+    output_polarity_tagger = call_polarity_tagger(URL_POLARITY_TAGGER,output_constituency_parse)
+    #print output_polarity_tagger
+
+    output_property_tagger = call_property_tagger(URL_PROPERTY_TAGGER,output_constituency_parse)
+    #print output_property_tagger
+
+    output_opinion_detector = call_opinion_detector(URL_OPINION_DETECTOR,output_constituency_parse)
+    #print output_opinion_detector
+
+
+    print "output_polarity_tagger result:"
+    get_polarity(output_polarity_tagger)
+
+    print "output_property_tagger result:"
+    get_polarity(output_property_tagger)
+
+    print "output_opinion_detector result:"
+    get_polarity(output_opinion_detector)
